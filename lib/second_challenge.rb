d48b671c57c485d659283b801df1b65e053a1623
Melissa Hummelt <mkhummelt@gmail.com>
2019-02-04 21:18:04 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,11 +5,16 @@ def second_challenge
       :patriarch => {},
       :matriach => {},
       :hero => {},
-      :hero_friends => []],
+      :hero_friends => [],
       },
     :capulet => {
       #your key/value pairs here!
-      }}
+      :patriach => {},
+      :matriach => {},
+      :heroine => {},
+      :heroine_friends => [],
+      }
+    }
 
 
 end
